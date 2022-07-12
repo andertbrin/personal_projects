b59d37aae74214f197e8837c621397868dc1956c
anderson.bringhenti <andersontb8@gmail.com>
2022-07-12 10:18:34 -0300

    creating a class

---
+++ github_copilot/test_class.py
@@ -0,0 +1,31 @@
+
+
+# crie uma classe
+class Car:
+    # crie um método __init__
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+        self.gas_tank = 0
+        self.gas_tank_size = 0
+        self.gas_tank_capacity = 0
+        self.gas_tank_level = 0
+        self.gas_tank_level_percentage = 0
+
+
+    # crie um método que mostre o carro
+    def describe_car(self):
+        print(f'This is a {self.year} {self.make} {self.model}.')
+
+    # crie um método que mostre o odômetro
+    def read_odometer(self):
+        print(f'This car has {self.odometer_reading} miles on it.')
+
+# crie duas instâncias da classe car
+car_1 = Car('Ford', 'Mustang', '2018')
+car_2 = Car('Toyota', 'Corolla', '2019')
+
+
+
